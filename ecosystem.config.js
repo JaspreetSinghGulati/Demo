c3c121b31c0b460fa105bdd3995c90920544a062
esjians <jaspreet.c.gulati@ericsson.com>
2020-06-24 12:43:33 +0530

    Ui-2312: development done

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'ENA-UI',
+      script    : './ui/index.js',
+      env: {
+       "NODE_ENV": "development"
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }	
+    },
+	{
+      name      : 'ENA-Service',
+      script    : './ena-server/build/index.js',
+      env: {
+       "NODE_ENV": "development"
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }	
+    }
+  ]
+};
